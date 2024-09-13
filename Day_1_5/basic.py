8601ff7846e2792fdd7d36aea466af33e0836791
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-09-13 11:51:48 +0530

    day 1 to 5 learning added

---
+++ Day_1_5/basic.py
@@ -0,0 +1,50 @@
+#Intro to python
+'''
+What is Python
+Features of Python
+Where is Python Used
+'''
+
+print("Hello World!")
+
+#Applications of Python
+'''
+1. Jarvis Virutal Assistant
+2. Love Calculator
+3. Face Recognition System
+4. Flappy Bird Game
+5. Snake Game
+'''
+
+#Modules and PIP
+'''
+Modules
+    built in & external modules.
+    
+'''
+
+#Our First Program
+print("Hello World!") 
+print(1+3, "is the ans")
+
+#print is a function which prints the value of the variables passed to it.
+#For Printing multi-line strings we use ''' --- ''' inside the print function.
+print('''
+hello
+how do you do
+this is an example
+for prinint multi line strings.
+check the code.
+''')
+
+# Comments, Escape Sequences & Print Statement
+print("This is an example \nof escape sequence")
+
+#Print statement has different paramaters to it.
+# Seperator sep, is used to add a seprator between the objects.
+# End, is used to specify what to print at the end.
+# file.
+# sep end file are optional paramaters.
+print("hey",23,2423, sep = "|", end = " concate 1192\n")
+print("end statement")
+
