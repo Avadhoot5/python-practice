ecfc6401dac8035788bcd81e6cfc838dd513cf36
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-14 09:19:03 +0530

    day 35 added

--- Day34_35/index.py
+++ Day34_35/index.py
@@ -46,3 +46,29 @@ del deleting[1] # used to delete the item using particular key
 # deleting.clear()
 
 print(deleting)
+
+# for Loop with else
+# IMPORTANT: else block will only be executed when for/while loop is succesfully executed for all values.
+# else executes when loop ends, not when loop breaks.
+
+for i in range(5):
+    print(i)
+else:
+    print("the loop has ended")
+
+for i in range(5):
+    print(i)
+    if i == 3:
+        break
+else:
+    print("The loop is broken")
+
+i = 0
+while i<5:
+    print(i)
+    i = i+1
+    if i>3:
+        break
+else:
+    print("while loop is done")
+
