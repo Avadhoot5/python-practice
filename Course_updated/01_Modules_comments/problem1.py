0ce39adc0abd49461c3feb6b5fb20f95a3796e2c
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-16 10:50:36 +0530

    practice set 1 solved

---
+++ Course_updated/01_Modules_comments/problem1.py
@@ -0,0 +1,40 @@
+import os
+
+# 1. print a poem
+
+poem = '''
+टूटे हुए तारों से फूटे बासंती स्वर
+पत्थर की छाती में उग आया नव अंकुर
+झरे सब पीले पात
+कोयल की कुहुक रात
+प्राची में अरुणिमा की रेख देख पाता हूं
+गीत नया गाता हूं।
+टूटे हुए सपने की सुने कौन सिसकी
+अंतर को चीर
+व्यथा पलकों पर ठिठकी
+हार नहीं मानूंगा, रार नहीं ठानूंगा
+काल के कपाल पर लिखता मिटाता हूं
+गीत नया गाता हूं।
+'''
+
+# print(poem)
+
+# 2. REPL, print table of 5
+
+def table(n):
+    for i in range(1, 11):
+        print(i * n)
+
+# table(5)
+
+# 3. external module
+# python3 -m pygame.examples.aliens
+
+# 4 print the contents of a directory using os module
+
+# gets the current working directory path
+cwd = os.getcwd()
+print(cwd)
+
+# prints the files in specific directory
+print(os.listdir(cwd))
