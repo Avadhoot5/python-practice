9494034ce458a276538196ed9b76ad306fb670a6
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-17 18:15:11 +0530

    variables and data types added

---
+++ Course_updated/02_Variables_DataTypes/01_variable.py
@@ -0,0 +1,8 @@
+a = 1
+b = 2.5
+c = 'test'
+
+print(a)
+print(b)
+print(c)
+
---
+++ Course_updated/02_Variables_DataTypes/02_datatypes.py
@@ -0,0 +1,10 @@
+a = 1 # a is integer 
+
+b = 2.2 # b is a floating point number
+
+c = 'test name' # c is a string
+
+boolVar = False # boolean variable
+
+d = None # d is a none type variable
+
