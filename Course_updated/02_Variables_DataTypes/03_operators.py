9423d88b83fcbda0e792be6de5394ed0fcd07614
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-17 18:41:11 +0530

    operators in python

---
+++ Course_updated/02_Variables_DataTypes/03_operators.py
@@ -0,0 +1,38 @@
+# Arithmetic operators
+
+a = 7
+b = 2
+add = a + b
+sub = a - b
+mul = a * b
+
+print('Arithmetic operators result', add, sub, mul)
+
+# Assignment operators
+
+c = 9 - 5
+d = 5
+d += 2
+
+print('Assignment operator result', c, d)
+
+# Comparison operators
+
+compRes = 5 < 4
+compRes2 = 2 == 2
+compRes3 = 5 >= 3
+notEqual = 2 != 2
+
+print('Comparison operators result')
+print(compRes,compRes2,compRes3)
+print(notEqual)
+
+# Logical operators
+
+orLogic = True or False
+andLogic = True and False
+notLogic = not(True)
+
+print('or ', orLogic)
+print('and ', andLogic)
+print('not ', notLogic)
