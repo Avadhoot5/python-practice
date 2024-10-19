df2ecc6c026e2748b3e525643e33d5a5873ac7c3
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-19 15:40:10 +0530

    intro to list

---
+++ Course_updated/04_List_Tuples/01_List.py
@@ -0,0 +1,11 @@
+fruitList = ['Apple', 'Orange', 5, 23.13, False]
+
+print(fruitList[0])
+
+# Lists are mutable
+fruitList[0] = 'banana'
+
+print(fruitList[0])
+
+
+
---
+++ Course_updated/contact.py
@@ -0,0 +1,12 @@
+# oneLadder = 1 step. Method = 1
+
+# twoLadder = [1, 1] / [2]. Method = 2 
+
+# 6th ladder = 
+# 5th + 4th
+# 4th 3rd + 3rd  2nd
+# 3rd 2nd 2nd 1st + 2nd 1st 1st
+# 2nd 1st 1st 0 + 1st 0 0 
+
+
+
