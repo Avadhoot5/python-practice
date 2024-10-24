0145b574593a5ae47ca5778073fce9d71899f666
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-24 19:11:56 +0530

    oops started

---
+++ Course_updated/11_OOPS/01_class.py
@@ -0,0 +1,10 @@
+class Employee :
+    name = 'Test'
+    language = 'Py'
+    salary = 1000000
+
+employee1 = Employee()
+
+print(employee1.name)
+print(employee1.language)
+
--- Course_updated/contact.py
+++ Course_updated/contact.py
@@ -9,3 +9,41 @@
 # 2nd 1st 1st 0 + 1st 0 0 
 
 
+
+# X power n 
+
+# 2 power 4  -> fn(x, n) => fn(constant, numberof times). 
+
+# def power(x, n):
+#     if (n == 0):
+#         return 1
+#     xn = power(x, n//2) * power(x, n//2)
+#     if (n % 2 == 0):
+#         return xn
+#     else:
+#         return x * xn
+
+
+# ans = power(2, 10)
+# print(ans)
+
+# def toh(n, id1, id2, id3):
+#     if (n == 0):
+#         return 1
+#     toh(n-1, id1, id3, id2) # move 2 discs recursively from A to C help of B
+#     print(f'{n} [{id1} -> {id2}]')
+#     toh(n-1, id3, id2, id1)
+
+# answer = toh(3, 'A', 'B', 'C')
+# print(answer)
+
+
+def printArr(n):
+    if (n == 0):
+        return
+    printArr(n-1)
+    print(n)
+
+printArr(50)
+
+
