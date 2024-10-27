28a90d6f7e6e37069abbacaf61bedbf892a696a8
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-27 18:02:59 +0530

    init method, constructors

--- Course_updated/11_OOPS/04_constructor.py
+++ Course_updated/11_OOPS/04_constructor.py
@@ -1,13 +1,10 @@
-# self keyword
-
 class Employee:
     language = 'Python'
     salary = 120000
 
-# TypeError: Employee.getInfo() takes 0 positional arguments but 1 was given
-    # def getInfo():
-    #print(f"The language is {language}, and the salary is {salary}")
-    
+    def __init__(self):
+        print('I"m creating an object')
+
     def getInfo(self):
         print(f"The language is {self.language}, and the salary is {self.salary}")
 
@@ -17,9 +14,8 @@ class Employee:
 
 
 test = Employee()
+test.name = 'firstName'
 test.language = 'javascript'
 
-test.getInfo()
-# the above is equivalent to Employee.getInfo(test)
-test.greet()
+print(test.name, test.salary)
 
