f71ea35e9eb12d2ab9a2ffb88ebd62cf398bcd64
Avadhoot Bhogil <58785159+Avadhoot5@users.noreply.github.com>
2024-10-29 12:18:10 +0530

    multiple inheritance added

--- Course_updated/12_Inheritance/02_typeInheritance.py
+++ Course_updated/12_Inheritance/02_typeInheritance.py
@@ -1,33 +1,51 @@
 # Multiple inheritance
 
-class Employee:
-    company = 'ITC'
-    name = 'Default name'
-    def show(self):
-        print(f'The name of the employee is {self.name} and the company is {self.company}')
+# class Employee:
+#     company = 'ITC'
+#     name = 'Default name'
+#     def show(self):
+#         print(f'The name of the employee is {self.name} and the company is {self.company}')
 
-class Coder:
-    language = 'python'
-    def printLanguages(self):
-        print(f'out of all languages here is your language: {self.language}')
+# class Coder:
+#     language = 'python'
+#     def printLanguages(self):
+#         print(f'out of all languages here is your language: {self.language}')
 
-class Programmer(Employee, Coder):
-    company = 'ITC Infotech'
-    def showLanguage(self):
-        print(f'The company is {self.company} and he is good with {self.language}')
+# class Programmer(Employee, Coder):
+#     company = 'ITC Infotech'
+#     def showLanguage(self):
+#         print(f'The company is {self.company} and he is good with {self.language}')
 
-a = Employee()
-b = Coder()
-c = Programmer()
+# a = Employee()
+# b = Coder()
+# c = Programmer()
 
-a.show()
-b.printLanguages()
+# a.show()
+# b.printLanguages()
 
-c.show()
-c.printLanguages()
-c.showLanguage()
+# c.show()
+# c.printLanguages()
+# c.showLanguage()
 
 # Multilevel inheritance
 
+class Employee:
+    a = 1
+
+class Programmer(Employee):
+    b = 2
+
+class Manager(Programmer):
+    c = 3
+
+o = Employee()
+print(o.a)
+# print(o.b) # throws an error as there is no b attribute in Employee class
+
+o = Programmer()
+print(o.a, o.b)
+
+o = Manager()
+print(o.a, o.b, o.c)
 
 
